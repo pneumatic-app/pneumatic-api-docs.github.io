ced536d25d1e18d42e262b7b98a46f09c0a6ed9d
rodionovstepan <rs@antidasoftware.com>
2020-07-13 16:26:26 -0500

    build slate to /docs

--- config.rb
+++ config.rb
@@ -39,6 +39,9 @@ end
 activate :relative_assets
 set :relative_links, true
 
+# Custom build path
+set :build_dir, 'docs'
+
 # Build Configuration
 configure :build do
   # We do want to hash woff and woff2 as there's a bug where woff2 will use
